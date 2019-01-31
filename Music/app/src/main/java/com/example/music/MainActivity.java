55a246406f5989dd64f33394c37c67bb27811328
Smriti1234 <smritimanay@gmail.com>
2019-01-31 15:16:57 +0530

    MUSIC APP

---
+++ Music/app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.music;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    int pausePosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.despa);
+            mediaPlayer.start();
+        } else {
+            mediaPlayer.seekTo(pausePosition);
+            mediaPlayer.start();
+        }
+    }
+
+    public void pause(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            pausePosition = mediaPlayer.getCurrentPosition();
+        }
+    }
+
+    public void stop(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer = null;
+
+        }
+
+    }
+}
